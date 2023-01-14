b7c5fda5d1ffcb155ea5be448e6ea4ad95c849c3
David Olaleye <olaoluwaolaleye@gmail.com>
2023-01-14 02:06:47 -0800

    Added a hamburger icon for responsiveness in smaller devices

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,45 +1,82 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { Sling as Hamburger } from "hamburger-react";
+
 import "../styles/layout.css";
 
 export default function Layout() {
+  const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
+
+  function handleHamburgerChange() {
+    return setIsOpen((prev) => !prev);
+  }
+
+  function home() {
+    navigate("/");
+  }
+
+  function hide() {
+    return setIsOpen(false);
+  }
+
   return (
     <nav className="nav">
-      <h1 className="layout_heading">Welcome</h1>
-
-      <div className="navigation">
-        <NavLink
-          to="/"
-          end
-          style={({ isActive }) => ({
-            color: isActive ? "tomato" : "white",
-            textDecoration: "none",
-            transition: "color 0.2s ease-in 0.2s",
-          })}
-        >
-          Home
-        </NavLink>{" "}
-        <NavLink
-          to="about"
-          style={({ isActive }) => ({
-            color: isActive ? "tomato" : "white",
-            textDecoration: "none",
-            transition: "color 0.2s ease-in 0.2s",
-          })}
-        >
-          ErrorBoundary
-        </NavLink>{" "}
-        <NavLink
-          to="repos"
-          style={({ isActive }) => ({
-            color: isActive ? "tomato" : "white",
-            textDecoration: "none",
-            transition: "color 0.2s ease-in 0.2s",
-          })}
-        >
-          Repos
-        </NavLink>
+      <div className="layout_heading" onClick={home}>
+        <h1 className="heading">Welcome</h1>
       </div>
+
+      <div className="hamburger-box">
+        <Hamburger
+          size={25}
+          color="#fff"
+          toggled={isOpen}
+          toggle={handleHamburgerChange}
+        />
+      </div>
+
+      <ul
+        className={isOpen ? "navigation-expand" : "navigation"}
+        onClick={hide}
+      >
+        <li className={isOpen ? "nav-el-expand" : ""}>
+          <NavLink
+            to="/"
+            end
+            style={({ isActive }) => ({
+              color: isActive ? "tomato" : "white",
+              textDecoration: "none",
+              transition: "color 0.2s ease-in 0.2s",
+            })}
+          >
+            Home
+          </NavLink>
+        </li>{" "}
+        <li className={isOpen ? "nav-el-expand" : ""}>
+          <NavLink
+            to="about"
+            style={({ isActive }) => ({
+              color: isActive ? "tomato" : "white",
+              textDecoration: "none",
+              transition: "color 0.2s ease-in 0.2s",
+            })}
+          >
+            ErrorBoundary
+          </NavLink>
+        </li>{" "}
+        <li className={isOpen ? "nav-el-expand" : ""}>
+          <NavLink
+            to="repos"
+            style={({ isActive }) => ({
+              color: isActive ? "tomato" : "white",
+              textDecoration: "none",
+              transition: "color 0.2s ease-in 0.2s",
+            })}
+          >
+            Repos
+          </NavLink>
+        </li>
+      </ul>
     </nav>
   );
 }
